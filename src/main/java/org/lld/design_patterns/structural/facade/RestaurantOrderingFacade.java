191f57060f61e3fc6640a71b3fafbea36e7944e1
Karthik <kartik.pawade@cognizant.com>
2024-05-22 12:52:00 +0530

    Structural Design Pattern : Facade Pattern.

---
+++ src/main/java/org/lld/design_patterns/structural/facade/RestaurantOrderingFacade.java
@@ -0,0 +1,17 @@
+package org.lld.design_patterns.structural.facade;
+
+public class RestaurantOrderingFacade {
+
+    private Object orderFoodItem(String foodItem) {
+        /**
+         *
+         * Dish dish = Helper.getWashedDish();
+         * if(Cook.checkIfFoodItemsAvailable()){
+         *    Food food = Cook.cookFood();
+         *    Cook.decorateFoodItem(Dish, food);
+         * return Dish;
+         * }
+         */
+        return null;
+    }
+}
